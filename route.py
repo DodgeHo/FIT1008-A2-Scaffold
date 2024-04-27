523f3604c11fbc91bd1db3c0ffd4d0cfd8ba94c5
Dodge (Lang HE) <asdsay@gmail.com>
2024-04-27 20:08:14 +0800

    1-9
    4
    
    3
    
    Once
    
    draft
    
    Revert "346"
    
    This reverts commit 33ff909765ed4451d7f0a807e69281490303db8d.
    
    Reapply "346"
    
    This reverts commit 6b213d08b3d5efa7b626e2ff55e258276e1c1a79.
    
    789
    
    7
    
    8
    
    Revert "789"
    
    This reverts commit 2e24ddaff7e74b0a6e136ebc887e2db6ebb69519.
    
    最后

--- route.py
+++ route.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
-
+from collections import deque
+from branch_decision import BranchDecision
 from computer import Computer
 
 from typing import TYPE_CHECKING, Union
@@ -26,7 +27,8 @@ class RouteSplit:
 
     def remove_branch(self) -> RouteStore:
         """Removes the branch, should just leave the remaining following route."""
-        raise NotImplementedError()
+        following_route = self.following
+        return following_route.store
 
 @dataclass
 class RouteSeries:
@@ -45,34 +47,39 @@ class RouteSeries:
         Returns a route store which would be the result of:
         Removing the computer at the beginning of this series.
         """
-        raise NotImplementedError()
+        following_route = self.following
+        return following_route.store
 
     def add_computer_before(self, computer: Computer) -> RouteStore:
         """
         Returns a route store which would be the result of:
         Adding a computer in series before the current one.
         """
-        raise NotImplementedError()
+        return RouteSeries(computer,Route(self))
 
     def add_computer_after(self, computer: Computer) -> RouteStore:
         """
         Returns a route store which would be the result of:
         Adding a computer after the current computer, but before the following route.
         """
-        raise NotImplementedError()
+        next_route = Route(RouteSeries(computer, self.following))
+        return RouteSeries(self.computer, next_route)
 
     def add_empty_branch_before(self) -> RouteStore:
         """Returns a route store which would be the result of:
         Adding an empty branch, where the current routestore is now the following path.
         """
-        raise NotImplementedError()
+        return RouteSplit(Route(None),Route(None),Route(self))
+
 
     def add_empty_branch_after(self) -> RouteStore:
         """
         Returns a route store which would be the result of:
         Adding an empty branch after the current computer, but before the following route.
         """
-        raise NotImplementedError()
+        next_following = RouteSplit(Route(None),Route(None), self.following)
+
+        return RouteSeries(self.computer, Route(next_following))
 
 
 RouteStore = Union[RouteSplit, RouteSeries, None]
@@ -88,19 +95,73 @@ class Route:
         Returns a *new* route which would be the result of:
         Adding a computer before everything currently in the route.
         """
-        raise NotImplementedError()
+        return Route(RouteSeries(computer, self))
 
     def add_empty_branch_before(self) -> Route:
         """
         Returns a *new* route which would be the result of:
         Adding an empty branch before everything currently in the route.
         """
-        raise NotImplementedError()
+        empty_branch = RouteSplit(
+            Route(None),
+            Route(None),
+            self
+        )
+        return Route(empty_branch)
 
     def follow_path(self, virus_type: VirusType) -> None:
         """Follow a path and add computers according to a virus_type."""
-        raise NotImplementedError()
+
+
+        rest_store_list = deque() 
+        rest_store_list.append(self.store)
+
+        while(len(rest_store_list)):
+            next_store = rest_store_list.pop()
+
+            if isinstance(next_store, RouteSplit):
+                decision = virus_type.select_branch(next_store.top, next_store.bottom)
+
+                following_store = next_store.following.store
+                rest_store_list.append(following_store)
+
+                if (decision == BranchDecision.TOP):
+                    next_store = next_store.top.store
+                elif (decision == BranchDecision.BOTTOM):
+                    next_store = next_store.bottom.store
+                else:
+                    #next_store = None
+                    break
+
+            elif isinstance(next_store, RouteSeries):
+                virus_type.add_computer(next_store.computer)
+                next_store = next_store.following.store
+
+            else:
+                next_store = None
+
+            if not next_store == None:
+                rest_store_list.append(next_store)
+
 
     def add_all_computers(self) -> list[Computer]:
         """Returns a list of all computers on the route."""
-        raise NotImplementedError()
+
+        computers_list = []
+        searching_list = [self.store]  
+
+        while len(searching_list) > 0:
+            current = searching_list.pop(0)
+
+            if isinstance(current, RouteSeries):
+                computers_list.append(current.computer)
+                if current.following.store!= None:
+                    searching_list.append(current.following.store)
+
+            elif isinstance(current, RouteSplit):
+                for pointer in [current.top.store, current.bottom.store, current.following.store]:
+                    if pointer != None:
+                        searching_list.append(pointer)
+
+
+        return computers_list
--- virus.py
+++ virus.py
@@ -61,7 +61,60 @@ class RiskAverseVirus(VirusType):
         """
         This virus is risk averse and likes to choose the path with the lowest risk factor.
         """
-        raise NotImplementedError()
+        top_route = type(top_branch.store) == RouteSeries
+        bot_route = type(bottom_branch.store) == RouteSeries
+
+        if top_route and bot_route:
+            
+            top_comp = top_branch.store.computer
+            bot_comp = bottom_branch.store.computer
+            both_zero_risk = False
+            # If it sees any computer with a risk factor of 0.0, it should take that path.
+            if top_comp.risk_factor == 0.0 and bot_comp.risk_factor != 0.0:
+                return BranchDecision.TOP
+            elif bot_comp.risk_factor == 0.0 and top_comp.risk_factor != 0.0:
+                return BranchDecision.BOTTOM
+            elif  top_comp.risk_factor == 0.0 and bot_comp.risk_factor == 0.0:
+                # If there are multiple computers with a risk_factor of 0.0, it should take the path with the lowest hacking difficulty.
+                if top_comp.hacking_difficulty != bot_comp.hacking_difficulty:
+                    if top_comp.hacking_difficulty > bot_comp.hacking_difficulty:
+                        return BranchDecision.TOP
+                    else:
+                        return BranchDecision.BOTTOM
+                    
+                both_zero_risk = True
+
+            # If there is still a tie, continue to the next comparisons.
+            # Take the highest value between the hacking_difficulty and the half of the hacked_value.
+
+            top_coef = max(top_comp.hacking_difficulty, 0.5 * top_comp.hacked_value)
+            bot_coef = max(bot_comp.hacking_difficulty, 0.5 * bot_comp.hacked_value)
+            # Then, divide this by the risk factor.
+            if not both_zero_risk:
+                # If the risk factor is zero, skip this step.
+                top_coef /= top_comp.risk_factor
+                bot_coef /= bot_comp.risk_factor
+
+            # Compare the two paths and take the path with the higher value.
+            if top_coef > bot_coef:
+                return BranchDecision.TOP
+            elif bot_coef > top_coef:
+                return BranchDecision.BOTTOM
+            
+            # If there is a tie, take the path with the lower risk factor.
+            if top_comp.risk_factor < bot_comp.risk_factor:
+                return BranchDecision.TOP
+            elif top_comp.risk_factor > bot_comp.risk_factor:
+                return BranchDecision.BOTTOM
+            
+            # If there is still a tie, then STOP.
+            return BranchDecision.STOP
+            
+        # If only one has a RouteSeries and the other a RouteSplit, pick the RouteSplit.
+        # In all other cases default to the Top path.
+        if top_route:
+            return BranchDecision.BOTTOM
+        return BranchDecision.TOP
 
 
 class FancyVirus(VirusType):
@@ -71,4 +124,55 @@ class FancyVirus(VirusType):
         """
         This virus has a fancy-pants and likes to overcomplicate its approach.
         """
-        raise NotImplementedError()
+        top_route = type(top_branch.store) == RouteSeries
+        bot_route = type(bottom_branch.store) == RouteSeries
+
+        if top_route and bot_route:
+            
+            threshold = self.calculate_threshold()
+            if (top_branch.store.computer.hacked_value < threshold):
+                return BranchDecision.TOP
+            elif (bottom_branch.store.computer.hacked_value < threshold):
+                return BranchDecision.BOTTOM
+            else:
+                return BranchDecision.STOP
+
+        # If only one has a RouteSeries and the other a RouteSplit, pick the RouteSplit.
+        # In all other cases default to the Top path.
+        if top_route:
+            return BranchDecision.BOTTOM
+        return BranchDecision.TOP
+    
+
+    def calculate_threshold(self) -> float:
+        """
+        Calculates the threshold by CALC_STR with reverse polish notation
+        """
+
+        chars = self.CALC_STR.split()
+        char_stack = []
+        
+        for char in chars:
+            if char.isdigit():  
+                # number push to the stack
+                char_stack.append(float(char)) 
+            else: 
+                # there should be 2 operands
+                b = char_stack.pop() 
+                a = char_stack.pop() 
+
+                if char == '+':
+                    char_stack.append(a + b)
+                elif char == '-':
+                    char_stack.append(a - b)
+                elif char == '*':
+                    char_stack.append(a * b)
+                elif char == '/':
+                    if b == 0:
+                        # If this happen, sth is wrong.
+                        raise ValueError("Zero Division.")
+                    char_stack.append(a / b)
+                    
+        
+        result = char_stack.pop()
+        return result
